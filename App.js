bd6925b9e7f13db99bb342d4522c5567165a1d87
James Line <durianlone@hotmail.co.uk>
2018-02-08 23:09:44 +1100

    Add files via upload
    Just a simple todo list.

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import Main from './app/components/Main'
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
